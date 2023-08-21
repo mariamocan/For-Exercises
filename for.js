455dd88d009b8d8428339be8dcef158184b5534d
Maria <maria.mocan.dev@gmail.com>
2023-08-21 15:12:06 +0300

    first commit

---
+++ for.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// for (let i = 0; i < 3; i++) {
+//   console.log("level 1");
+//   for (let j = 0; j < 2; j++) {
+//     console.log("level 2");
+//   }
+// }
+
+// const arr = [2, 4, 5, 6, 7];
+
+// for (let i = arr.length - 1; i > 0; i = i - 2) {
+//   console.log(i, arr[i]);
+// }
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    console.log(i);
+    break;
+  }
+}
