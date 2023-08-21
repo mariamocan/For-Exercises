0dcbef5ccd3343ee84a7b6b194fe858bdc496bea
Maria <maria.mocan.dev@gmail.com>
2023-08-21 22:02:32 +0300

    another way

--- for-ex.js
+++ for-ex.js
@@ -55,11 +55,8 @@ for (let i = 13; i < 1000; i++) {
 // Count the occurrences of 4 from the array [10, 4, 8, 4, 7, 4, 29, 4, 99] and console.log them using a message like: “There are x occurrences of 4 in the array”.
 
 const occurrences = [10, 4, 8, 4, 7, 4, 29, 4, 99];
-let elementCount = {};
 
-for (let i = 0; i < occurrences.length; i++) {
-  if (i === 4) {
-    elementCount = i;
-  }
-}
-console.log(`There are ${elementCount} occurrences of 4 in the array`);
+const numberFilter = 4;
+
+let count = occurrences.filter((elem) => elem == numberFilter).length;
+console.log(`There are ${count} occurrences of 4 in the array`);
