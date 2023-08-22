8920f02b73521cdf5ba9054c7f4059e2293611cf
Maria <maria.mocan.dev@gmail.com>
2023-08-22 21:12:17 +0300

    in progress

--- for-ex.js
+++ for-ex.js
@@ -1,3 +1,4 @@
+"use strict";
 // You have the array: [1, 20, 5, 60, 7, 14, 8].
 // a) Use a for loop to console.log only the numbers greater than 10.
 // b) Use an array method (function) to do the same thing from a).
@@ -37,14 +38,15 @@ console.log(evenNumbers);
 const firstArray = [1, 20, 5, 60, 10, 7, 14, 8];
 const secondArray = [1, 2, 3];
 
-function arrayParameter(...first) {
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] === 10)
-      console.log(first[i], `You have the number 10 in the array`);
-    else if (first[i] % 2 === 0) console.log(first[i]);
+function printByCondition(...array) {
+  let odd = "";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 10)
+      console.log(array[i], `You have the number 10 in the array`);
+    else if (array[i] % 2 === 0) console.log(array[i]);
   }
 }
-arrayParameter(...firstArray);
+printByCondition(...firstArray);
 
 // Print in the console the numbers from the interval [0, 1000] that divide by 13 (split by dot . or contains dot . or use the modulo % operator).
 
@@ -54,9 +56,58 @@ for (let i = 13; i < 1000; i++) {
 
 // Count the occurrences of 4 from the array [10, 4, 8, 4, 7, 4, 29, 4, 99] and console.log them using a message like: “There are x occurrences of 4 in the array”.
 
+//Variant1
 const occurrences = [10, 4, 8, 4, 7, 4, 29, 4, 99];
 
 const numberFilter = 4;
 
 let count = occurrences.filter((elem) => elem == numberFilter).length;
-console.log(`There are ${count} occurrences of 4 in the array`);
+console.log(`There are ${count} occurrences of ${numberFilter} in the array`);
+
+//VAriant 2
+const occurrences1 = [10, 4, 8, 4, 7, 4, 29, 4, 99];
+const searchElement = 4;
+const count1 = countOccurrences(occurrences1, searchElement);
+
+function countOccurrences(array, searchElement) {
+  let count = 0;
+  for (let element of array) if (element === searchElement) count++;
+  return count;
+}
+console.log(`There are ${count} occurrences of ${searchElement} in the array`);
+
+//Remove all 4 from the array [10, 4, 8, 4, 7, 4, 29, 4, 99].
+
+const removeArr = [10, 4, 8, 4, 7, 4, 29, 4, 99];
+for (let i = 0; i < removeArr.length; i++) {
+  if (removeArr[i] === 4) continue;
+  console.log(removeArr[i]);
+}
+
+//Replace all 4 with 999 in the array [10, 4, 8, 4, 7, 4, 29, 4, 99].
+
+const replaceArr = [10, 4, 8, 4, 7, 4, 29, 4, 99];
+const newNumber = 999;
+for (let i = 0; i < replaceArr.length; i++) {
+  if (replaceArr[i] === 4) replaceArr[i] = newNumber;
+}
+
+console.log(replaceArr);
+
+// Create a function that has an array as a parameter. The function should return true if the array has more than 3 occurrences of 7. Use both approaches with for and while. Example: for [10, 7, 8, 4, 7, 4, 29, 4, 99] the function returns false, [1, 2, 3] also returns false, but for [10, 7, 8, 7, 4, 7, 4, 29, 7, 4, 7, 7, 99] the function returns true.
+
+const paramArray = [10, 7, 8, 4, 7, 4, 29, 4, 99];
+const paramArray1 = [1, 2, 3];
+const paramArray2 = [10, 7, 8, 7, 4, 7, 4, 29, 7, 4, 7, 7, 99];
+
+//While approach
+
+function checkOccurrences(...array) {
+  const seven = 7;
+
+  const newCheck = array.filter((elem) => elem == seven).length;
+  while (newCheck > 3) return true;
+  return false;
+}
+
+console.log(checkOccurrences(...paramArray2));
